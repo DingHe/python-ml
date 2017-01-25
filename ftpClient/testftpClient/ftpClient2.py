37dc5ddfde6d367037493bec9196843f12733d74
DingHeHsiao <positivecat2011@gmail.com>
2017-01-25 16:59:59 +0800

    ftpClient2

--- ftpClient/testftpClient/ftpClient2.py
+++ ftpClient/testftpClient/ftpClient2.py
@@ -4,11 +4,7 @@ import ftplib
 from ftplib import FTP
 import subprocess
 from time import sleep
-import os
-from unzipandmv import gunzip
-from unzipandmv import mvfile
 import re
-import random
 
 timeout=30
 HOST=""
@@ -16,12 +12,10 @@ PORT=21
 USER=''
 PASSWD=''
 DISTPATH="/usr1/data4g"   #flume spooling directory
-SUBPROC="/usr1/xdr/unzipandmv.py"
+SUBPROC="/usr1/xdr/putdata117.sh"
 
 def matchfilename(filename):
     return re.match('.*http.*.gz$', filename)
-    
- 
 
 def ftpget(remotepath,localpath):
     ftp=FTP()
@@ -48,37 +42,16 @@ def ftpget(remotepath,localpath):
                 continue
             localfile=localpath+'/'+f
             ftp.retrbinary('RETR ' + f,open(localfile,'wb').write)
-            fno=random.randint(0,4)
-            if fno == 0:
-                subprocess.Popen([SUBPROC,localfile,DISTPATH])
-            else:
-                subprocess.Popen([SUBPROC,localfile,DISTPATH+str(fno)])        
+            subprocess.Popen([SUBPROC,localfile])     
             ftp.delete(f)
-
-def unzipAndMoveLocalfile(localpath,distdir):
-    localfiles=os.listdir(localpath)
-    for f in localfiles:
-        localfile=localpath+'/'+f
-        print "localfilename=%s" % localfile
-        if os.path.isdir(localfile):
-            pass
-        elif f[-4:] == '.txt':
-            mvfile(localfile,"/usr1/data4g")
-        elif f[-3:] == '.gz':
-            try:
-                gunzip(localfile)
-            except Exception:
-                print 'gunzip failed,deleted'
-                os.remove(localfile)
 
 if __name__ == '__main__':
     remotepath='/'
-    localpath='/usr1/xdr'
-    unzipAndMoveLocalfile(localpath,DISTPATH)      
+    localpath='/usr1/xdr'   
     while True:
         try:
             ftpget(remotepath, localpath)
         except Exception:
             print 'hapen error,get files error'
-    #print matchfilename("S1u-hainan-20170124184039-20170124184039-021001.txt.gz")
+
 
